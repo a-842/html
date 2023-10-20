d8e9031cb6a0ceded77d76a67512e1644be6a0cc
Ainsworth Coultart <Ainsworth@Trevor--Rachaels-iMac-2.local>
2023-10-20 11:36:17 +0100

    Project Page 1

--- blog-script.js
+++ blog-script.js
@@ -1,13 +1,13 @@
 const posts = [
     {
-        title: "Post 1",
-        content: "Content of Post 1",
+        title: "Sample Text",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         date: "2023-10-10",
         project: "Project A"
     },
     {
-        title: "Post 2",
-        content: "Content of Post 2",
+        title: "What I like",
+        content: "orange",
         date: "2023-10-15",
         project: "Project B"
     },
@@ -23,10 +23,12 @@ function renderBlogPosts(posts) {
         const postElement = document.createElement("div");
         postElement.className = "post";
         postElement.innerHTML = `
-            <h2>${post.title}</h2>
-            <p>${post.content}</p>
-            <p>Date: ${post.date}</p>
-            <p>Project: ${post.project}</p>
+            <div class="post-box">
+                <h2>${post.title}</h2><p>${post.date}</p>
+                <p>${post.content}</p>
+                <p>Project: ${post.project}</p>
+            </div>
+            <br>
         `;
         postWall.appendChild(postElement);
     });
@@ -45,6 +47,12 @@ function filterByProject(project) {
 
 // Create project filter buttons
 const projects = [...new Set(posts.map(post => post.project))]; // Extract unique project names
+
+const showAllButton = document.createElement("button");
+showAllButton.textContent = "All";
+showAllButton.addEventListener("click", () => renderBlogPosts(posts));
+projectList.appendChild(showAllButton);
+
 projects.forEach(project => {
     const button = document.createElement("button");
     button.textContent = project;
